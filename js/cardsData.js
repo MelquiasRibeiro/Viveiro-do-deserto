bf7f58d5d9ab442d1b2e37fb3103b4289d5cd022
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-06-10 15:54:01 -0300

    feat: initial commit :fire:

---
+++ js/cardsData.js
@@ -0,0 +1,84 @@
+let cardsData = [{
+  id: 1,
+  image: './assets/flores/akira.jpg',
+  user: { 
+    title: 'Akira' 
+  },
+},
+{
+  id: 1,
+  image: './assets/flores/akira.jpg',
+  user: { 
+    title: 'Akira' 
+  },
+},
+{
+  id: 1,
+  image: './assets/flores/akira.jpg',
+  user: { 
+    title: 'Akira' 
+  },
+},{
+  id: 1,
+  image: './assets/flores/akira.jpg',
+  user: { 
+    title: 'Akira' 
+  },
+},{
+  id: 1,
+  image: './assets/flores/akira.jpg',
+  user: { 
+    title: 'Akira' 
+  },
+},{
+  id: 1,
+  image: './assets/flores/akira.jpg',
+  user: { 
+    title: 'Akira' 
+  },
+},{
+  id: 1,
+  image: './assets/flores/akira.jpg',
+  user: { 
+    title: 'Akira' 
+  },
+},
+]
+
+//  Lm .37
+//  Lm 25
+//  Ariel perfumada ->>>>>>>essa
+//  Ev 139
+//  Cleopatra ----------->>>>>essa
+//  Turandot
+//  Esther coral
+//  Blue boy
+//  Dourada
+//  Peach cobbler
+//  Vanila
+//  Uva dobrada
+//  Barbie girl
+//  Golden faith
+//  Ts 301      ----->>>>essa
+//  Ploc .2
+//  Steel
+//  Bridal bouquet
+//  Princesinha do sol
+//  Paradise
+//  Màh Bièë
+//  Sindy
+//  Coração Roxo
+//  Julio Cesar
+//  BMT Estrelada
+//  Amarela dobrada forte
+//  Branca de neve tripla
+//  Mini pendente
+//  Akira
+//  Ts 30
+//  Swallow
+//  Indigo glaze
+//  Lm 16
+//  Egg roll
+//  Santa claus
+//  Miss vietnam
+//  Red Leaf
